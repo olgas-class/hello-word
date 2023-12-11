b4c0e41178b3c13aa97f62a099962f16994092e2
olga_demina <olga.demina1410@gmail.com>
2023-12-11 11:37:03 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+// Qui va scritto codice php
+$name = 'Michele';
+$age = 25;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Hello, <?php echo $name; ?>, from php</h1>
+</body>
+
+</html>
