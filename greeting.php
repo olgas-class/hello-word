cfd0f14ef32cfce140f98e7fa0a5e0d1cca2a7aa
olga_demina <olga.demina1410@gmail.com>
2023-12-11 12:13:49 +0100

    add: form handling with get

---
+++ greeting.php
@@ -0,0 +1,20 @@
+<?php
+$username = $_GET['username'];
+$lastname = $_GET['lastname'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Ciao <?php echo $username; ?> <?php echo $lastname; ?></h1>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // Qui va scritto codice php
 // $name = 'Michele';
 // $age = 25;
-$name = $_GET['name']
+// $name = $_GET['name']
 ?>
 
 <!DOCTYPE html>
@@ -15,7 +15,22 @@ $name = $_GET['name']
 </head>
 
 <body>
-    <h1>Hello, <?php echo $name; ?>, from php</h1>
+    <!-- <h1>Hello, 
+        <?php // echo $name; ?>
+    , from php</h1> -->
+
+    <h1>Dimmi il tuo nome e io ti saluto</h1>
+
+    <form action="greeting.php" method="GET">
+        <label for="username">Tuo nome</label>
+        <input type="text" id="username" name="username">
+
+        <label for="lastname">Tuo cognome</label>
+        <input type="text" id="lastname" name="lastname">
+
+        <button type="submit">Invia</button>
+    </form>
+
 </body>
 
 </html>
